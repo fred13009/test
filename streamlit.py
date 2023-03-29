1891f9613b2d3275ef3c60076ea5efcff5c26c81
fred13009 <129304992+fred13009@users.noreply.github.com>
2023-03-29 17:48:01 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -12,23 +12,23 @@ st.title('Advertising Budget Allocator')
 total_budget = st.number_input("Enter the total advertising budget:", value=1.0, step=0.01, format="%.2f")
 
 # Get user input for the number of channels
-num_channels = st.number_input("Enter the number of advertising channels:", min_value=1, value=1, step=1, format="%d")
+num_channels = st.number_input("Enter the number of advertising channels:", min_value=1, value=1, step=1, format="%d", key="num_channels")
 
 with st.form("channel_form"):
     # Get user input for channel details
     ad_channels = {}
     for i in range(num_channels):
         with st.expander(f"Channel {i+1}"):
-            channel_name = st.text_input(f"Enter the name of channel {i+1}: ")
-            return_on_investment = st.number_input(f"Enter the return on investment (ROI) for {channel_name}:", value=1.0, step=0.01, format="%.2f")
+            channel_name = st.text_input(f"Enter the name of channel {i+1}: ", key=f"channel_name_{i}")
+            return_on_investment = st.number_input(f"Enter the return on investment (ROI) for {channel_name}:", value=1.0, step=0.01, format="%.2f", key=f"roi_{i}")
             
-            min_revenue = st.text_input(f"Enter the minimum revenue constraint for {channel_name} (leave blank for none): ")
+            min_revenue = st.text_input(f"Enter the minimum revenue constraint for {channel_name} (leave blank for none): ", key=f"min_revenue_{i}")
             min_revenue = float(min_revenue) if min_revenue.strip() else None
             
-            min_budget = st.text_input(f"Enter the minimum budget constraint for {channel_name} (leave blank for none): ")
+            min_budget = st.text_input(f"Enter the minimum budget constraint for {channel_name} (leave blank for none): ", key=f"min_budget_{i}")
             min_budget = float(min_budget) if min_budget.strip() else None
             
-            max_budget = st.text_input(f"Enter the maximum budget constraint for {channel_name} (leave blank for none): ")
+            max_budget = st.text_input(f"Enter the maximum budget constraint for {channel_name} (leave blank for none): ", key=f"max_budget_{i}")
             max_budget = float(max_budget) if max_budget.strip() else None
 
             ad_channels[channel_name] = {
@@ -39,9 +39,9 @@ with st.form("channel_form"):
             }
 
     # Get user input for the optional channel exposure constraint
-    use_channel_exposure_constraint = st.selectbox("Do you want to use a channel exposure constraint?", options=["no", "yes"])
+    use_channel_exposure_constraint = st.selectbox("Do you want to use a channel exposure constraint?", options=["no", "yes"], key="exposure_constraint")
     if use_channel_exposure_constraint == "yes":
-        min_channels_used = st.number_input("Enter the minimum number of channels to be used:", min_value=1, value=1, step=1, format="%d")
+        min_channels_used = st.number_input("Enter the minimum number of channels to be used:", min_value=1, value=1, step=1, format="%d", key="min_channels_used")
     else:
         min_channels_used = 0
 
