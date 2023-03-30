6de1d711357a3c4edd6c4cec4af4b03c70b5936e
fred13009 <129304992+fred13009@users.noreply.github.com>
2023-03-30 09:43:59 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -17,23 +17,28 @@ with st.form(key='input_form'):
     
     # Get user input for the number of channels
     num_channels = st.number_input("Enter the number of advertising channels:", value=1, step=1, min_value=1, format="%d")
-    
+
+    # Create a placeholder for the input fields
+    channel_inputs_placeholder = st.empty()
+
     # Get user input for channel details
     ad_channels = {}
     for i in range(num_channels):
-        st.write(f"Channel {i + 1}:")
-        channel_name = st.text_input(f"Enter the name of channel {i + 1}:")
-        return_on_investment = st.number_input(f"Enter the return on investment (ROI) for {channel_name}:", value=1.0, step=0.01, format="%.2f", key=f"roi_{i}")
-        min_revenue = st.number_input(f"Enter the minimum revenue constraint for {channel_name} (leave blank for none):", value=-1, key=f"min_revenue_{i}")
-        min_budget = st.number_input(f"Enter the minimum budget constraint for {channel_name} (leave blank for none):", value=-1, key=f"min_budget_{i}")
-        max_budget = st.number_input(f"Enter the maximum budget constraint for {channel_name} (leave blank for none):", value=-1, key=f"max_budget_{i}")
+        channel_inputs = channel_inputs_placeholder.container()
+        with channel_inputs:
+            st.write(f"Channel {i + 1}:")
+            channel_name = st.text_input(f"Enter the name of channel {i + 1}:")
+            return_on_investment = st.number_input(f"Enter the return on investment (ROI) for {channel_name}:", value=1.0, step=0.01, format="%.2f", key=f"roi_{i}")
+            min_revenue = st.number_input(f"Enter the minimum revenue constraint for {channel_name} (leave blank for none):", value=-1, key=f"min_revenue_{i}")
+            min_budget = st.number_input(f"Enter the minimum budget constraint for {channel_name} (leave blank for none):", value=-1, key=f"min_budget_{i}")
+            max_budget = st.number_input(f"Enter the maximum budget constraint for {channel_name} (leave blank for none):", value=-1, key=f"max_budget_{i}")
 
-        ad_channels[channel_name] = {
-            "return_on_investment": return_on_investment,
-            "min_revenue": min_revenue if min_revenue != -1 else None,
-            "min_budget": min_budget if min_budget != -1 else None,
-            "max_budget": max_budget if max_budget != -1 else None,
-        }
+            ad_channels[channel_name] = {
+                "return_on_investment": return_on_investment,
+                "min_revenue": min_revenue if min_revenue != -1 else None,
+                "min_budget": min_budget if min_budget != -1 else None,
+                "max_budget": max_budget if max_budget != -1 else None,
+            }
 
     # Get user input for the optional channel exposure constraint
     use_channel_exposure_constraint = st.radio("Do you want to use a channel exposure constraint?", ("No", "Yes"))
