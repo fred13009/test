b90a764a04219d523228202cbabed1554b1ddb13
fred13009 <129304992+fred13009@users.noreply.github.com>
2023-04-12 13:10:15 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,136 +1,45 @@
 import streamlit as st
-import pandas as pd
-import pulp
+from optimizer import optimize_budget
 
-def _get_state():
-    if 'state' not in st.session_state:
-        st.session_state['state'] = {}
-    return st.session_state['state']
+def app():
+    st.set_page_config(page_title='Marketing Budget Allocation')
 
-def step1(state):
-    st.header("Step 1: Enter Overall Budget")
+    st.title('Marketing Budget Allocation')
 
-    with st.form(key="step1_form"):
-        overall_budget = st.number_input("Overall Budget", min_value=0.0, step=0.01)
-        next_step1 = st.form_submit_button("Next")
+    channels = []
+    channel_count = st.number_input('Number of Channels', min_value=1, max_value=10, value=1)
+    for i in range(channel_count):
+        channel_name = st.text_input(f'Channel {i + 1} Name')
+        channel_roi = st.number_input(f'Historical ROI for Channel {i + 1}', min_value=0.0)
+        channel_min_budget = st.number_input(f'Minimum Budget for Channel {i + 1}', min_value=0.0)
+        channel_max_budget = st.number_input(f'Maximum Budget for Channel {i + 1}', min_value=0.0)
+        channel_targeted_sales = st.number_input(f'Targeted Sales for Channel {i + 1} (optional)', min_value=0.0, value=0.0)
+        channels.append({
+            'name': channel_name,
+            'roi': channel_roi,
+            'minBudget': channel_min_budget,
+            'maxBudget': channel_max_budget,
+            'targetedSales': channel_targeted_sales
+        })
 
-    if next_step1:
-        state['overall_budget'] = overall_budget
-        state['step'] = 2
+    total_budget = st.number_input('Total Marketing Budget', min_value=0.0)
 
-def step2(state):
-    st.header("Step 2: Enter Number of Channels")
-
-    with st.form(key="step2_form"):
-        num_channels = st.number_input("Number of Channels", min_value=2, step=1)
-        next_step2 = st.form_submit_button("Next")
-
-    if next_step2:
-        state['num_channels'] = num_channels
-        state['step'] = 3
-        state['channel_data'] = {i: {"name": "", "roas": 1.1} for i in range(1, num_channels + 1)}
-
-def step3(state):
-    st.header("Step 3: Enter Channel Names and Historical ROAS")
-
-    num_channels = state['num_channels']
-    channel_data = state['channel_data']
-
-    with st.form(key="step3_form"):
-        for i in range(1, num_channels + 1):
-            col1, col2 = st.columns(2)
-            with col1:
-                channel_name = st.text_input(f"Channel {i} Name", key=f"channel_name_{i}")
-            with col2:
-                channel_roas = st.number_input(f"Channel {i} ROAS", min_value=0.0, step=0.01, value=1.1, key=f"channel_roas_{i}")
-
-            channel_data[i]['name'] = channel_name
-            channel_data[i]['roas'] = channel_roas
-
-        next_step3 = st.form_submit_button("Next")
-
-    if next_step3:
-        state['step'] = 4
-        state['channel_data'] = channel_data
-
-def step4(state):
-    st.header("Step 4: Set Constraints")
-
-    with st.form("constraints_form"):
-        selected_channel = st.selectbox("Select Channel", options=[c['name'] for c in state['channel_data']])
-        constraint_type = st.selectbox("Select Constraint Type", options=["Minimum Budget Constraint", "Maximum Budget Constraint", "Minimum Revenue Constraint"])
-        constraint_value = st.number_input("Enter Constraint Value", min_value=0.0, step=0.01)
-
-        add_constraint = st.form_submit_button("Set a New Constraint")
-        submit_calculation = st.form_submit_button("Submit for Calculation")
-
-    if add_constraint:
-        state['constraints'].append((selected_channel, constraint_type, constraint_value))
-        st.write(f"Added {constraint_type} for {selected_channel}: {constraint_value}")
-        st.experimental_rerun()
-
-    if submit_calculation:
-        state['step'] = 5
-        st.experimental_rerun()
-
-def step5(state):
-    st.header("Optimized Budget Allocation")
-
-    overall_budget = state['overall_budget']
-    channel_data = {state['channel_data'][i]['name']: state['channel_data'][i]['roas'] for i in state['channel_data']}
-    constraints = state['constraints']
-
-    budget_allocation, expected_revenue = allocate_budget(channel_data, constraints, overall_budget)
-
-    st.write(pd.DataFrame(budget_allocation.items(), columns=["Channel", "Budget Allocation"]))
-
-    st.header("Expected Optimized Overall Revenue")
-    st.write(expected_revenue)
-
-    even_split_budget = overall_budget / len(channel_data)
-    even_split_revenue = sum([even_split_budget * channel_data[channel] for channel in channel_data.keys()])
-
-    st.header("Revenue if Budget was Split Evenly Across Channels")
-    st.write(even_split_revenue)
-
-def allocate_budget(channel_data, constraints, overall_budget):
-    prob = pulp.LpProblem("AdvertisingBudgetAllocation", pulp.LpMaximize)
-
-    budget_vars = pulp.LpVariable.dicts("Budget", channel_data.keys(), lowBound=0, cat='Continuous')
-
-    for channel, constraint_type, constraint_value in constraints:
-        if constraint_type == "Minimum Budget Constraint":
-            prob += budget_vars[channel] >= constraint_value
-        elif constraint_type == "Maximum Budget Constraint":
-            prob += budget_vars[channel] <= constraint_value
-        elif constraint_type == "Minimum Revenue Constraint":
-            prob += budget_vars[channel] * channel_data[channel] >= constraint_value
-
-    prob += pulp.lpSum([budget_vars[channel] for channel in channel_data.keys()]) <= overall_budget
-
-    prob += pulp.lpSum([budget_vars[channel] * channel_data[channel] for channel in channel_data.keys()])
-
-    prob.solve()
-
-    budget_allocation = {channel: budget_vars[channel].varValue for channel in channel_data.keys()}
-    expected_revenue = pulp.value(prob.objective)
-
-    return budget_allocation, expected_revenue
-
-st.title("Advertising Budget Allocation")
-
-state = _get_state()
-
-if 'step' not in state:
-    state['step'] = 1
-
-if state['step'] == 1:
-    step1(state)
-elif state['step'] == 2:
-    step2(state)
-elif state['step'] == 3:
-    step3(state)
-elif state['step'] == 4:
-    step4(state)
-elif state['step'] == 5:
-    step5(state)
+    if st.button('Calculate Allocation'):
+        numeric_channels = []
+        for channel in channels:
+            numeric_channels.append({
+                'name': channel['name'],
+                'roi': float(channel['roi']),
+                'minBudget': float(channel['minBudget']),
+                'maxBudget': float(channel['maxBudget']),
+                'targetedSales': float(channel['targetedSales'])
+            })
+        result = optimize_budget(numeric_channels, float(total_budget))
+        if result['success']:
+            st.header('Optimal Budget Allocation')
+            allocation_table = []
+            for i, channel_allocation in enumerate(result['allocation']):
+                allocation_table.append([channels[i]['name'], f'${channel_allocation:,.2f}'])
+            st.table(allocation_table)
+        else:
+            st.error(result['message'])
